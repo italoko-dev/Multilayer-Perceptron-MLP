0fc0d879380696bb31b519dca6684b716cba5d14
ITALO PIOVAN <italo_piovan@hotmail.com>
2021-12-03 20:10:23 -0300

    Update MLP.py
    Feito de forma estática.

--- MLP.py
+++ MLP.py
@@ -1,65 +1,124 @@
 import numpy as np
+from numpy.core.numeric import isclose
 
-#Inicializa os pesos (valores aleatórios)
-def initialize_parameters(layer_dims):
-    parameters = {}
-    for i in range(1,len(layer_dims)):
-        parameters['W' + str(i)] = np.random.rand(layer_dims[i],layer_dims[i])
-        #parameters['b' + str(i)] = np.random.rand(layer_dims[i],layer_dims[i]) 
-    return parameters
+class Mlp:
+  def __init__(self):
+      self.parameters = {}
+      self.eta = 1
 
+  #Inicializa os pesos (valores aleatórios)
+  def initialize_parameters(self):
+    parameters = {
+      "W1": np.random.rand(2,2),
+      "W2": np.random.rand(2,2)
+    }
+    self.parameters = parameters
 
-def sigmoid(x):
-  return 1/(1 + np.exp(-x))
+  def sigmoid(self,x):
+    return 1/(1 + np.exp(-x))
 
-#Derivate
-def de_sigmoid(x):
-    f = sigmoid(x)
+  #Derivate
+  def de_sigmoid(self,x):
+    f = self.sigmoid(x)
     return f * (1-f)
 
-def net(input,weights):
+  def net(self,input,weights):
     return  np.sum(np.multiply(input,weights))
 
-def l_layer_forwardProp(input, parameters, layer_dims):
-  i = input
- 
-  for layer in range(1,layer_dims[]):
-    i_prev = i
-    i = sigmoid(net(i_prev,parameters['W' + str(layer)]))
+  def forward(self,input):
+    i_result = {
+      "iC" : self.sigmoid(np.sum(np.multiply(input,self.parameters["W1"][0,:]))),
+      "iD" : self.sigmoid(np.sum(np.multiply(input,self.parameters["W1"][1,:])))}
+
+    i_result["iS1" ] = self.sigmoid(np.sum(np.multiply(i_result["iC"],self.parameters["W2"][0,:])))
+    i_result["iS2" ] = self.sigmoid(np.sum(np.multiply(i_result["iD"],self.parameters["W2"][1,:])))
+    
+    return i_result
+
+  def calc_error(self,i_result,y,expected):
+    error_s1 = (expected[y,0] - i_result["iS1"]) * self.de_sigmoid(i_result["iS1"])
+    error_s2 = (expected[y,1] - i_result["iS2"]) * self.de_sigmoid(i_result["iS2"])
+
+    gradient_errors = {
+      "gradient_s1" : error_s1,
+      "gradient_s2" : error_s2,
+      "gradient_c"  : error_s1 * self.parameters["W2"][:,0] + error_s2 * self.parameters["W2"][:,0],
+      "gradient_d"  : error_s1 * self.parameters["W2"][:,1] + error_s2 * self.parameters["W2"][:,1]       
+    }  
+    return gradient_errors
+
+  def setting_weights(self,errors,i_results):
+    self.parameters["W2"] = self.parameters["W2"][0,:] + self.eta * errors["gradient_s1"] * i_results["iS1"]
+    self.parameters["W2"] = self.parameters["W2"][1,:] + self.eta * errors["gradient_s2"] * i_results["iS2"]
+    
+    self.parameters["W1"] = self.parameters["W1"][0,:] + self.eta * errors["gradient_c"] * i_results["iC"]
+    self.parameters["W1"] = self.parameters["W1"][1,:] + self.eta * errors["gradient_d"] * i_results["iD"]
+
+  def calc_error_network(s_errors):
+    return 0.5 * (np.sum(s_errors))**2
 
 
-  return A
+  def backpropagation():
+    ##Implementar
+
+"""
+pesos = np.array([
+        #w1
+        [0.5, 1.0],  # [(a,c) , (a,d)]
+        [-0.7, 1.2] # [(b,c) , (b,d)]  
+        ])
+"""
+
+#input = np.array([1,0])
+#print(pesos[:,1])
 
 
+#
 
+mlp = Mlp()
+params = mlp.initialize_parameters()
+#print(params["W1"])
+#print("----------------------")
+#print(params["W1"][0,:])
+#print(params["W1"][1,:])
 
-   
+#print(len(params["W1"][1]))
 
+expected = np.array(
+  [
+    [1,0],
+    [0,1]
+  ])
+print(expected[1,1])
 
+"""
+netC = np.sum(np.multiply(input,params["W1"][0,:]))
+iC = mlp.sigmoid(netC)
+netD = np.sum(np.multiply(input,params["W1"][1,:]))
+iD = mlp.sigmoid(netD)
 
+netS1 = np.sum(np.multiply(netC,params["W2"][0,:]))
+iS1 = mlp.sigmoid(netS1)
+netS2 = np.sum(np.multiply(netD,params["W2"][0,:]))
+iS2 = mlp.sigmoid(netS2)
 
+print (f"NetC: {netC}")
+print (f"NetD: {netD}")
+print (f"NetS1: {netS1}")
+print (f"NetS2: {netS2}")
 
-def teste(layer_dims):
-    print (initialize_parameters(layer_dims))
+print (f"iC: {iC}")
+print (f"iD: {iD}")
+print (f"iS1: {iS1}")
+print (f"iS2: {iS2}")
 
+"""
+#test = np.sum(np.multiply(entrada,pesos[:1])) 
 
-#teste([2,2,2])
-
-params = initialize_parameters([2,2,2])
-
-
-entrada = np.array([1,0])
-oculta = params["W1"]
-
-netC = np.sum(np.multiply(entrada,oculta))
-netD = np.sum(np.multiply(entrada,oculta))
-
-
-
-print(entrada) 
-print(oculta)
-print(netC)
-print(len(params))
+#print(entrada) 
+#print(oculta)
+#print(netC)
+#print(test)
 
 #1 - Aplica as entradas X1, X2, ..., Xn 
 #2 - Calcula os nets da camada oculta
